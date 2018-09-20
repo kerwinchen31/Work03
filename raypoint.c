384bb251d03f9fc3305f91eedc08a5c72b39f3b7
kerwinchen31 <kchen37@stuy.edu>
2018-09-19 23:21:16 -0400

    added hw

--- raypoint.c
+++ raypoint.c
@@ -1,11 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main(){
+  
   srand( time(NULL) );
   int ray[10];
+  int rye[10];
+  
   for (int i = 0; i < 10; i++){
     ray[i] = rand();
   }
+  ray[9] = 0;
 
+  printf("values in array: \n");
+  for (int i = 0; i < 10; i++){
+    printf("%d\n", ray[i]);
+  }
+
+  for(int i = 0; i < 10; i++)
+    *(rye + i) = *(ray + 9 - i);
+
+  printf("backwards, from new array: \n");
+  for (int i = 0; i < 10; i++){
+    printf("%d\n", rye[i]);
+  }
 }
